447f9d55adfd119e4f58818d8c9417db22b5fe5a
mandrei100 <mcrisandreica@hotmail.com>
2019-10-15 08:54:39 -0400

    Made some changes

--- src/callbacker.js
+++ src/callbacker.js
@@ -1,16 +1,26 @@
-const syncCallbacker = () => {
-    
+const syncCallbacker = (res, rej) => {
+    let argsArray = [...arguments];
+    if (argsArray.length < 2){
+        throw error;
+    }
+    if (typeof res !== 'function' || typeof rej !== 'function'){
+        throw error;
+    }
+    return res(rej());
 };
 
 
-const asyncCallbacker = (a,b) => {
-    let data = null;
-    let dun = data => {
-        b((data, dun)=>{
-            a((data, dun));
-        })
-    };
-   // a((data, dun))
+const asyncCallbacker = (res, rej) => {
+    let argsArray = [...arguments];
+    if (argsArray.length < 2){
+        throw error;
+    }
+    if (typeof res !== 'function' || typeof rej !== 'function'){
+        throw error;
+    }
+    return res = (data,done) => {
+        rej (data,done);
+    }
 };
 
 
